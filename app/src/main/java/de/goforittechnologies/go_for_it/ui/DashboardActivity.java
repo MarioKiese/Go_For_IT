3e9ba1a0d4b25b23e0a3a5d1bcc78569e4895bf5
Toha1997 <hammerbachertom@gmx.de>
2018-11-29 21:15:57 +0100

    Adding methods toDashBoard Activity with BarChart - Adding method to draw barchart - adding method to build up entryList out of List<double[])

--- app/src/main/java/de/goforittechnologies/go_for_it/ui/DashboardActivity.java
+++ app/src/main/java/de/goforittechnologies/go_for_it/ui/DashboardActivity.java
@@ -35,11 +35,11 @@ public class DashboardActivity extends AppCompatActivity {
             inputList.add(day);
             day = new double[24];
         }
-        List<BarEntry> entries = buildDayAddedBarChart(inputList,0,30);
+        List<BarEntry> entries = buildMonthAddedBarChart(inputList,0,30);
         invalitadeBarCHart(entries);
 
     }
-    private List<BarEntry> buildDayAddedBarChart(List<double[]> inputList, int minDay, int maxDay){
+    private List<BarEntry> buildMonthAddedBarChart(List<double[]> inputList, int minDay, int maxDay){
         List<BarEntry> entries = new ArrayList<>();
         double value = 0;
         if (minDay < 0){
@@ -54,11 +54,23 @@ public class DashboardActivity extends AppCompatActivity {
                 value =+ inputList.get(i)[j];
                 entries.add(new BarEntry((float)i,(float)value));
             }
+            i++;
         }
         return entries;
-
-
     }
+    private List<BarEntry> buildDayBarChart(List<double[]> inputList, int day){
+        List<BarEntry> entries = new ArrayList<>();
+        double value = 0;
+        for (int i = 0; i< 24;i++){
+            entries.add(new BarEntry((float)i,(float)inputList.get(day)[i]));
+        }
+        return entries;
+    }
+    private List<BarEntry> buildWeekBarChart(List<double[]> inputList, int minDay, int maxDay){
+        List<BarEntry> entries = new ArrayList<>();
+        return entries;
+    }
+
     private void invalitadeBarCHart(List<BarEntry> inputList){
         BarDataSet set = new BarDataSet(inputList, "BarDataSet");
         BarData data = new BarData(set);
