456e3bf595370f80df1db32532c54abf3fedb1e9
Toha1997 <hammerbachertom@gmx.de>
2018-11-29 20:30:00 +0100

    Adding methods toDashBoard Activity with BarChart - Adding method to draw barchart - adding method to build up entryList out of List<double[])

--- app/src/main/java/de/goforittechnologies/go_for_it/ui/DashboardActivity.java
+++ app/src/main/java/de/goforittechnologies/go_for_it/ui/DashboardActivity.java
@@ -15,32 +15,47 @@ import de.goforittechnologies.go_for_it.R;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    private BarChart barChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        barChart= (BarChart) findViewById(R.id.barChart);
 
-        // in this example, a LineChart is initialized from xml
-        BarChart barChart = (BarChart) findViewById(R.id.barChart);
+        
 
 
+
+    }
+    private List<BarEntry> buildDayAddedBarChart(List<double[]> inputList, int minDay, int maxDay){
         List<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0f, 30f));
-        entries.add(new BarEntry(1f, 80f));
-        entries.add(new BarEntry(2f, 60f));
-        entries.add(new BarEntry(3f, 50f));
-        // gap of 2f
-        entries.add(new BarEntry(5f, 70f));
-        entries.add(new BarEntry(6f, 60f));
+        double value = 0;
+        if (minDay < 0){
+            minDay = 0;
+        }
+        if (maxDay >= inputList.size() ){
+            maxDay = inputList.size();
+        }
+        int i = minDay;
+        while(i < maxDay){
+            for (int j = 0; j<24;j++){
+                value =+ inputList.get(i)[j];
+                entries.add(new BarEntry((float)i,(float)value));
+            }
+        }
+        return entries;
 
-        BarDataSet set = new BarDataSet(entries, "BarDataSet");
 
+    }
+
+    private void invalitadeBarCHart(List<BarEntry> inputList){
+
+        BarDataSet set = new BarDataSet(inputList, "BarDataSet");
         BarData data = new BarData(set);
         data.setBarWidth(0.9f); // set custom bar width
         barChart.setData(data);
         barChart.setFitBars(true); // make the x-axis fit exactly all bars
         barChart.invalidate(); // refresh
     }
-
 
 }
